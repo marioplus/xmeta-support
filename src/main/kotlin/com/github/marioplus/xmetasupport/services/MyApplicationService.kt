df6c0b651e2c81729dd66ffec90cd8734eea669c
GitHub Action <action@github.com>
2022-06-21 14:08:31 +0000

    Template cleanup

--- src/main/kotlin/com/github/marioplus/xmetasupport/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/marioplus/xmetasupport/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.marioplus.xmetasupport.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.marioplus.xmetasupport.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/marioplus/xmetasupport/services/MyProjectService.kt
+++ src/main/kotlin/com/github/marioplus/xmetasupport/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.marioplus.xmetasupport.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.marioplus.xmetasupport.MyBundle
 
 class MyProjectService(project: Project) {
 
